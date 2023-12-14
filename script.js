9cf97f4a4a01ae95d4e80b3057630455cb62ce90
negisuraj080 <143009358+negisuraj080@users.noreply.github.com>
2023-12-14 13:25:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function toCase(text) {
   // write your code here
+	if (!str.trim()) {
+    return '-';
+  }
+
+  // Convert the string to lowercase and uppercase
+  const lowerCaseStr = str.toLowerCase();
+  const upperCaseStr = str.toUpperCase();
+
+  // Join the lowercase and uppercase strings with a hyphen
+  const result = `${lowerCaseStr}-${upperCaseStr}`;
+
+  return result;
 }
 
 // DO not change the code below
